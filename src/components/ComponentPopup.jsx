1e9815dd7b250378201e82b1dd0f13d26303160b
Maria <madr0002@stud.kea.dk>
2024-10-28 19:51:31 +0100

    med tailwind

--- src/components/ComponentPopup.jsx
+++ src/components/ComponentPopup.jsx
@@ -4,16 +4,16 @@ import { IoMdClose } from "react-icons/io";
 
 const ComponentPopup = () => {
     return ( 
-        <div className="popup">
-            <div className="grid">
-            <FaCookieBite size={40}/>
-            <IoMdClose/>
+        <div class="grid grid-rows-3, grid-cols-1 bg-black text-white rounded-xl fixed bottom-5 right-5 h-64 w-56">
+            <div className="flex flex-col space-y-2">
+            <div className="flex justify-end px-2 py-2"><IoMdClose/></div>
+            <div class="pl-4"><FaCookieBite size={50}/></div>
             </div>
-        <div>
+        <div className="pl-4">
            <p>We use cookies to improve your user experience.</p>
         </div>
-        <div>
-        <button className="button" class="rounded bg-zinc-50 text-black px-10 py-1">
+        <div class="grid m-auto	">
+        <button class="rounded bg-zinc-50 text-black px-10 py-1">
             I like Cookies
         </button>
         </div>
